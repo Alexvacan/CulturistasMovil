d9077f9926b2a52c8d913f085de79b1a088b94b4
melanytorrez <torrezmelany13@gmail.com>
2026-03-29 22:55:41 -0400

    Implementacion de la pantalla de gestion dede espacios

--- composeApp/src/commonMain/kotlin/com/easypark/app/di/PresentationModule.kt
+++ composeApp/src/commonMain/kotlin/com/easypark/app/di/PresentationModule.kt
@@ -1,4 +1,9 @@
 package com.easypark.app.di
 
+import com.easypark.app.spacemanagement.presentation.viewmodel.SpaceManagementViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
-val presentationModule = module { /* Aquí irán tus ViewModels más adelante */ }
+
+val presentationModule = module {
+    viewModelOf(::SpaceManagementViewModel)
+}
